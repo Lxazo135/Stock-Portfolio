a2e76ee0a53d10dde82d9e56a5f1256c1a9f5641
Lan Pham <lxazo135@gmail.com>
2017-12-01 00:23:09 -0500

    Testing with service and file writing

--- app/src/main/java/edu/temple/phamstockportfolio/MainActivity.java
+++ app/src/main/java/edu/temple/phamstockportfolio/MainActivity.java
@@ -2,8 +2,12 @@ package edu.temple.phamstockportfolio;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -12,14 +16,21 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity implements PortfolioFragment.StockSelectable {
 
+    public StockDetailsService server;
     public FragmentManager fm;
     public PortfolioFragment nav;
     public Fragment details;
     public EditText editText;
     public Button button;
     String symbolFromIntent = "";
+    final String HTTP = "http://dev.markitondemand.com/MODApis/Api/v2/Quote/json/?symbol=";
+    final String FILE_NAME = "Stocks.txt";
+    String json;
+    ServiceConnection serverConnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +45,22 @@ public class MainActivity extends AppCompatActivity implements PortfolioFragment
                 add(R.id.viewFrame,nav).
                 commit();
 
+        serverConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                StockDetailsService.LocalBinder binder = (StockDetailsService.LocalBinder)iBinder;
+                server = binder.getServerInstance();
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+
+            }
+        };
+
+        Intent serviceIntent = new Intent(this, StockDetailsService.class);
+        this.bindService(serviceIntent, serverConnection,Context.BIND_AUTO_CREATE);
+        this.startService(serviceIntent);
     }
 
     @Override
@@ -44,6 +71,27 @@ public class MainActivity extends AppCompatActivity implements PortfolioFragment
             symbolFromIntent = intent.getStringExtra("symbol");
             System.out.println("SYMBOLZZZZ: " + symbolFromIntent);
             nav.addSymbol(symbolFromIntent);
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    json = server.getJson(HTTP + symbolFromIntent);
+                }
+            });
+            thread.start();
+
+            try {
+                System.out.println("JSON FROM SERVER: " + json);
+                server.writeJsonToFile(json, FILE_NAME);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            String jsonFromFile = server.readJsonFromFile(FILE_NAME);
+            System.out.println("$$$$$ JSON FROM FILE: " + jsonFromFile);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
---
+++ app/src/main/java/edu/temple/phamstockportfolio/StockDetailsService.java
@@ -0,0 +1,111 @@
+package edu.temple.phamstockportfolio;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class StockDetailsService extends Service {
+
+    IBinder mBinder = new LocalBinder();
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        System.out.println("SERVICE START");
+        return START_NOT_STICKY;
+    }
+
+    public class LocalBinder extends Binder {
+        public StockDetailsService getServerInstance() {
+            return StockDetailsService.this;
+        }
+    }
+
+    public String getJson(String http){
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+        StringBuffer buffer = new StringBuffer();
+
+        try{
+            URL url = new URL(http);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+            InputStream stream = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+            String line = "";
+            while((line = reader.readLine())!= null){
+                buffer.append(line + "\n");
+                Log.d("Response: ", line);
+            }
+
+            //return buffer.toString();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(connection != null){
+                connection.disconnect();
+            }
+            try{
+                if (reader != null){
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return buffer.toString();
+    }
+
+    public void writeJsonToFile(String json, String fileName) throws IOException {
+
+        File stockFile = new File(this.getFilesDir(), fileName);
+        if(!stockFile.exists()){
+            stockFile.createNewFile();
+        }
+        FileWriter fw = new FileWriter(stockFile);
+        fw.append(json);
+        fw.append("\n");
+        fw.flush();
+        fw.close();
+    }
+
+    public String readJsonFromFile(String fileName) throws IOException {
+        String filePath = this.getFilesDir() + "/" + fileName;
+        File file = new File(filePath);
+
+        FileInputStream fis = this.openFileInput(fileName);
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader bufferedReader = new BufferedReader(isr);
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            sb.append(line);
+        }
+
+        return sb.toString();
+    }
+}
